c5b2e8a2a3f54d1a6adf113b9520fcaf19c72aea
Gaurang1979 <126887545+Gaurang1979@users.noreply.github.com>
2026-09-02 00:09:45 +0530

    feat: add direct Hikvision scheduler hooks

---
+++ biometric_integration/hooks.py
@@ -0,0 +1,16 @@
+app_name = "biometric_integration"
+app_title = "Biometric Integration"
+app_publisher = "Sundaram Technologies"
+app_description = "Direct Hikvision biometric device integration for ERPNext HRMS"
+app_email = ""
+app_license = "MIT"
+
+scheduler_events = {
+    "cron": {
+        "*/10 * * * *": [
+            "biometric_integration.biometric_integration.hikvision.sync_all_devices"
+        ]
+    }
+}
+
+fixtures = []
